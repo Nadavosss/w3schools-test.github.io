bc7cd95c2f08f59f33f942683a1e578e21612083
Nadavosss <lenadavoss@gmail.com>
2025-03-30 16:45:31 +0300

    Added 2 new files to the fork!

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("My name is Nadav!\n");
+    printf("I'm 21 years old.\n");
+
+    printf("In WW2, the Japanese performed horrible experiments on Chinese POWs.\n
+            Amongst those experiments, they discovered that the human body is made\n
+            of 70 percents water.\n
+            Yeah, that's how we discovered that. Kinda creepy...\n");
+    return 0;
+}
